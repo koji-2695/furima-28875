971b0b860550c04e06a8385e562fbf22eb836a2d
koji-2695 <70596242+koji-2695@users.noreply.github.com>
2020-12-30 16:31:16 +0900

    0だとうまくいかないバリデーションのテストコードなど

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  
+
+  before do
+    @list = FactoryBot.build(:list)
+  end
 
   describe '商品出品' do
 
     context '商品出品がうまくいくとき' do
 
       it "商品名と商品の説明、カテゴリー、商品の状態、配送料の負担、発送元の地域、発送までの日数、販売価格が存在すれば登録できる" do
-        @list = FactoryBot.build(:list)  
+        
         expect(@list).to be_valid
       end
 
@@ -17,13 +20,13 @@ RSpec.describe List, type: :model do
       
 
       it "販売価格は半角数字であれば登録できる" do
-        @list = FactoryBot.build(:list)  
+        
         @list.price = "50000"
         expect(@list).to be_valid
       end
 
       it "販売価格は300~9999999の範囲であれば登録できる" do
-        @list = FactoryBot.build(:list)  
+         
         @list.price = "7777777"
         expect(@list).to be_valid
       end
@@ -36,65 +39,106 @@ RSpec.describe List, type: :model do
     context '商品出品がうまくいかないとき' do
 
       it "商品名が空だと登録できない" do
-        @list = FactoryBot.build(:list)  
+        
         @list.item_name = ""  
         @list.valid?
         expect(@list.errors.full_messages).to include("Item name can't be blank")
       end
 
+      
+
       it "商品の説明が空だと登録できない" do
-        @list = FactoryBot.build(:list)  
+        
         @list.explanation = ""  
         @list.valid?
         expect(@list.errors.full_messages).to include("Explanation can't be blank")
       end
 
+      
+
       it "カテゴリーが空だと登録できない" do
-        @list = FactoryBot.build(:list)  
+        
         @list.category_id = ""  
         @list.valid?
         expect(@list.errors.full_messages).to include("Category can't be blank")
       end
 
+      it "カテゴリーが0だと登録できない" do
+        
+        @list.category_id = "0"  
+        @list.valid?
+        expect(@list.errors.full_messages).to include("Category must be other than 0")
+      end
+
+
+
       it "商品の状態が空だと登録できない" do
-        @list = FactoryBot.build(:list)  
+        
         @list.condition_id = ""  
         @list.valid?
         expect(@list.errors.full_messages).to include("Condition can't be blank")
       end
 
+      it "商品の状態が0だと登録できない" do
+        
+        @list.condition_id = "0"  
+        @list.valid?
+        expect(@list.errors.full_messages).to include("Condition must be other than 0")
+      end
+
       it "配送料の負担が空だと登録できない" do
-        @list = FactoryBot.build(:list)  
+        
         @list.shipping_fee_id = ""  
         @list.valid?
         expect(@list.errors.full_messages).to include("Shipping fee can't be blank")
       end
 
+      it "配送料の負担が0だと登録できない" do
+        
+        @list.shipping_fee_id = "0"  
+        @list.valid?
+        expect(@list.errors.full_messages).to include("Shipping fee must be other than 0")
+      end
+
       it "発送元の地域が空だと登録できない" do
-        @list = FactoryBot.build(:list)  
+        
         @list.area_id = ""  
         @list.valid?
         expect(@list.errors.full_messages).to include("Area can't be blank")
       end
 
+      it "発送元の地域が0だと登録できない" do
+        
+        @list.area_id = "0"  
+        @list.valid?
+        expect(@list.errors.full_messages).to include("Area must be other than 0")
+      end
+
       it "発送までの日数が空では登録できない" do
-        @list = FactoryBot.build(:list)  
+        
         @list.shipping_date_id = ""  
         @list.valid?
         expect(@list.errors.full_messages).to include("Shipping date can't be blank")
       end
 
+      it "発送までの日数が0では登録できない" do
+        
+        @list.shipping_date_id = "0"  
+        @list.valid?
+        expect(@list.errors.full_messages).to include("Shipping date must be other than 0")
+      end
+
       
       
       it "販売価格は半角数字以外は登録できない" do
-        @list = FactoryBot.build(:list) 
+        
         @list.price = "ab"
         @list.valid?
         expect(@list.errors.full_messages).to include("Price is not included in the list")
       end
 
       it "販売価格は300~9999999の範囲でなければ登録できない" do
-        @list = FactoryBot.build(:list) 
+        
         @list.price = "3333333333"
         @list.valid?
         expect(@list.errors.full_messages).to include("Price is not included in the list")
