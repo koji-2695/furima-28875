840878cb447251714caff94cc241864060d652ab
koji-2695 <70596242+koji-2695@users.noreply.github.com>
2021-01-08 16:41:47 +0900

    商品出品機能の修正

--- app/models/list.rb
+++ app/models/list.rb
@@ -7,37 +7,19 @@ class List < ApplicationRecord
 
   validates_inclusion_of :price, in: 300..9_999_999
   
-  with_options presence: true, numericality: { only_integer: true } do
-  validates :price
-
-  validates :item_name
-
-  validates :explanation
-
-  validates :image
-
-
-  validates :category_id
-
-  validates :condition_id
-
-  validates :shipping_fee_id
-
-  validates :area_id
-
-  validates :shipping_date_id
-
-  end
-
-  with_options numericality: { other_than: 0 } do
-   
-    
-    validates :category_id
-    validates :condition_id
-    validates :shipping_fee_id
-    validates :area_id
-    validates :shipping_date_id
-
+  with_options presence: true do
+    validates :price
+    validates :item_name
+    validates :explanation
+    validates :image
+   validates :category_id
+    with_options numericality: { other_than: 0 , only_integer: true} do
+      validates :category_id
+      validates :condition_id
+      validates :shipping_fee_id
+      validates :area_id
+      validates :shipping_date_id
+    end
   end
 
 
